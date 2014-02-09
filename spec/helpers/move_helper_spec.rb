daf54a3b5f879dbc02a02489da40071044f718fb
Steven Barragan <me@steven.mx>
2014-02-09 17:00:54 -0600

    Create home view and controller

---
+++ spec/helpers/move_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+# Specs in this file have access to a helper object that includes
+# the MoveHelper. For example:
+#
+# describe MoveHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+describe MoveHelper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
