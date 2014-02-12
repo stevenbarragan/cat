684daef1183e6ca42ee18dceb5f3a3547b7dd248
Steven Barragan <me@steven.mx>
2014-02-12 07:03:47 -0600

    Add controller tests

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe HomeController do
-
+  it 'resnders the index view' do
+    get :index
+    expect(response).to render_template :index end
 end
--- spec/controllers/move_controller_spec.rb
+++ spec/controllers/move_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe MoveController do
+  it 'render a json' do
+    post :create, moves: []
 
+    body = JSON.parse(response.body)
+    expect(body["next"]).to be_true
+  end
+
+  it 'render a status in json' do
+    post :create, moves: [1,2,3,4,5,6,7,8]
+
+    body = JSON.parse(response.body)
+    expect(body["status"]).to be_true
+  end
 end
